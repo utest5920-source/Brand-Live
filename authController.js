b7ab6c9b33873bb0f63660eff0555faaeb81324c
utest5920-source <utest5920@gmail.com>
2025-08-25 18:00:26 +0530

    Create authController.js

---
+++ authController.js
@@ -0,0 +1,22 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+exports.register = async (req, res) => {
+  const { name, email, password, role, Logintype } = req.body;
+  const hash = await bcrypt.hash(password, 10);
+  const user = new User({ name, email, password: hash, role, Logintype });
+  await user.save();
+  res.status(201).json({ message: "User registered" });
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) return res.status(400).json({ message: "Invalid email" });
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) return res.status(400).json({ message: "Invalid password" });
+
+  const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET);
+  res.json({ token });
+};
