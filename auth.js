4b3313629ca9831d44252ae9dc2ffae5b44c84da
utest5920-source <utest5920@gmail.com>
2025-08-25 17:55:39 +0530

    Create auth.js
    new

---
+++ auth.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const { register, login } = require("../controllers/authController");
+
+router.post(
+    "/register", 
+    register
+);
+router.post(
+    "/login", 
+    login
+);
+
+module.exports = router;
